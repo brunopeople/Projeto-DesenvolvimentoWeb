26177690500548d7d1c5629c8637edac191f7186
bruno pessoa nunes de melo <brunopessoa.melo@gmail.com>
2021-01-14 17:07:39 -0300

    criando os componente filhos do departament e employee

--- admincrud/src/app/app.module.ts
+++ admincrud/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartamentComponent } from './departament/departament.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { ShowDepComponent } from './departament/show-dep/show-dep.component';
+import { AddEditDepComponent } from './departament/add-edit-dep/add-edit-dep.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DepartamentComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    ShowDepComponent,
+    AddEditDepComponent,
+    ShowEmpComponent
   ],
   imports: [
     BrowserModule,
